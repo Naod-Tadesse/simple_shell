b671f1ad9017422f8ea5e2987448bc0447b27d32
Joseph Omoruwou <josephomoruwou@gmail.com>
2023-09-20 12:08:13 +0100

    added function to convert argument to integer

---
+++ _atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts string to integer
+ * @string: pointer to string
+ *
+ * Return: integer
+ */
+int _atoi(char *string)
+{
+	unsigned long int num = 0;
+	int i = 0;
+
+	if (string[0] != '+')
+	{
+	}
+	else
+	{
+		string++;
+	}
+	while (string[i] != '\0')
+	{
+		if (string[i] >= '0' && string[i] <= '9')
+		{
+			num = num * 10 + (string[i] - '0');
+			if (num > INT_MAX)
+			{
+				return (-1);
+			}
+			i++;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (num);
+}
