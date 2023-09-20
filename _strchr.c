fa25d95e603d6d689c895895c9a4732cae33d1ef
Naod Taddese <naodtds@gmail.com>
2023-09-20 16:11:13 +0300

    code for searching string

---
+++ _strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - This function returns a pointer of the first occurenc of char
+ * @s: string
+ * @c: char
+ * Return: NULL or ponter to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
